fb9c1fec51bc17e8b9742643c1a03c46f85308cf
dr0id007 <nullvoid.nv@gmail.com>
2020-01-31 16:59:53 +0530

    prod server init

--- client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-import "./style/chat.css";
-import "./style/messages.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
